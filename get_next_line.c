12cf494bcc64d7832dcd667cab8eb91c432a33ac
TTR0NC0 <Oscarmr632@gmail.com>
2021-11-22 11:27:55 +0100

    ACTS

--- get_next_line.c
+++ get_next_line.c
@@ -1,23 +1,45 @@
 
 
 #include "get_next_line.h"
-/*
-static int strlen(char *str1)
+
+static int ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while()
+	while(str[i] != 0)
+		i++;
+	return (0);
 }
 
-static char *strjoin(char *line, char *str)
+static char *ft_strjoin(char *last, char *str)
 {
 	int	i;
+	char *line;
+	int	j;
 
+	i = ft_strlen(last);
+	i += ft_strlen(str);
+	j = 0;
+	line = (char *) malloc(sizeof(char) * i + 1);
+	if (!line)
+		return (0);
+	line[i] = 0;
 	i = 0;
+	while(last[i] != 0)
+	{
+		line[i] = last[i];
+		i++;
+	}
+	while(str[j] != 0 && str[j] != '\n')
+	{
+		line[i] = str[j];
+		i++;
+		j++;
+	}
+	return (line);
 }
 
-*/
 char *get_next_line(int fd)
 {
 	char *str;
@@ -36,6 +58,7 @@ char *get_next_line(int fd)
 	while (xd == size)
 	{
 		xd = read(fd, str, size);
+		str[xd] = 0;
 		//str[xd] = 0;
 		//strjoin();
 		aux += xd;
@@ -52,6 +75,7 @@ int main(void)
 	char *str;
 
 	fd = open("LINES.txt", O_RDONLY);
+	while
 	str = get_next_line(fd);
 	printf("%s", str);
 	free(str);
