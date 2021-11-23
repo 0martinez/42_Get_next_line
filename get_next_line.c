7ced50331b29f6420ac757ef2336059e066fdd5b
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-11-23 21:12:36 +0100

    ACTUALIZACIONES

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,18 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:51:57 by omartine          #+#    #+#             */
+/*   Updated: 2021/11/23 21:11:37 by omartine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int ft_strlen(char *str)
+static int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -12,14 +22,13 @@ static int ft_strlen(char *str)
 	return (0);
 }
 
-static char *ft_strjoin(char *last, char *str)
+static char	*ft_strjoin(char *last, char *str)
 {
-	int	i;
-	char *line;
-	int	j;
+	int		i;
+	char	*line;
+	int		j;
 
 	i = ft_strlen(last);
-	printf("%d", i);
 	i += ft_strlen(str);
 	j = 0;
 	line = (char *) malloc(sizeof(char) * i + 1);
@@ -27,12 +36,12 @@ static char *ft_strjoin(char *last, char *str)
 		return (0);
 	line[i] = 0;
 	i = 0;
-	while(last[i] != 0)
+	while (last[i] != 0)
 	{
 		line[i] = last[i];
 		i++;
 	}
-	while(str[j] != 0 && str[j] != '\n')
+	while (str[j] != 0 && str[j] != '\n')
 	{
 		line[i] = str[j];
 		i++;
@@ -41,51 +50,86 @@ static char *ft_strjoin(char *last, char *str)
 	return (line);
 }
 
-char *get_next_line(int fd)
+static int	check_container(const char *line)
 {
-	char *str;
-	static char *line;
-	int	xd;
-	int aux;
+	int	i;
+
+	i = 0;
+	while (line[i] != 0)
+	{
+		if (line[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static char	*ft_return(int i, const char *line)
+{
+	static int		j;
+	char			*str;
+
+	j = 0;
+	str = malloc(sizeof(char) * i + 1);
+	while (j < i)
+	{
+		str[j] = line[j];
+		j++;
+	}
+	str[j] = 0;
+	j++;
+	return (str);
+}
+
+static char	*ft_adeblas(const char *line)
+{
+	static int		i;
+
+	i = 0;
+	while (line[i] != '\n')
+		i++;
+	i++;
+	return (ft_return(i - 1, line));
+}
+
+char	*get_next_line(int fd)
+{
+	char		*str;
+	static char	*line;
+	int			xd;
+	int			aux;
 
 	xd = BUFFER_SIZE;
 	aux = 0;
+	//ft_str para retornar el trozo de tu container y le asignas un nuevo valor sin tu str
+	/*if (check_container(line) == 1)
+		return (ft_adeblas(line));*/
 	str = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!str)
 		return (0);
-	while (xd > 0)
+	while (xd > 0 && check_container(line) != 1)
 	{
 		xd = read(fd, str, BUFFER_SIZE);
 		str[xd] = 0;
-		//printf("|%s|", str);
 		line = ft_strjoin(line, str);
-		//free(str);
-		aux += xd;
-		//printf("+%d+", xd);
 	}
-	//xd = read(fd, str, BUFFER_SIZE);
-	//str[xd] = 0;
-	line[aux] = 0;
-	//ls = xd;
-	return (line);
+	free(str);
+	return (0);
 }
 
 int main(void)
 {
-    int	fd;
-	int xd = 0;
-	char *str;
-	fd = open("LINES.txt", O_RDONLY);
-	printf("//%d//", BUFFER_SIZE);
+	int		fd;
+	int		xd = 0;
+	char	*str;
+	fd = open("LINES", O_RDONLY);
 	str = get_next_line(fd);
-	while(str != 0)
+	while (str != 0)
 	{
 		printf("-%s-", str);
 		free(str);
 		str = get_next_line(fd);
 	}
-	//str = get_next_line(fd);
-	//printf("-%s-", str);
 	free(str);
 	close(fd);
 	return (0);
