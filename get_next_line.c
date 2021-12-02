5343d937c48ddfe4ea13890cde375a3903eab420
Oscar Martinez Rodriguez <omartine@c1r4s3.42madrid.com>
2021-12-02 20:15:11 +0100

    FUCK LEAKS

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: omartine <omartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:35:34 by omartine          #+#    #+#             */
-/*   Updated: 2021/11/25 20:44:38 by omartine         ###   ########.fr       */
+/*   Updated: 2021/12/02 20:06:27 by omartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i] != 0)
 		i++;
 	return (i);
@@ -30,6 +32,8 @@ static char	*ft_strjoin(char *container, char *str)
 
 	i = 0;
 	j = 0;
+	if (!str)
+		return (0);
 	if (container[0] != 0)
 		i = ft_strlen(container);
 	i += ft_strlen(str);
@@ -59,6 +63,8 @@ static int	check_container(char *container)
 	int	i;
 
 	i = 0;
+	if (container[0] == 0)
+		return (0);
 	while (container[i] != 0)
 	{
 		if (container[i] == '\n')
@@ -71,7 +77,6 @@ static int	check_container(char *container)
 static char	*ft_line_return(char *container, int jump)
 {
 	int		i;
-	int		j;
 	char	*line_return;
 
 	i = 0;
@@ -109,6 +114,34 @@ static char	*new_container(char *container, int jump)
 	return (new_container);
 }
 
+static char	*adeblas(char *str)
+{
+	int		i;
+	char	*to_return;
+
+	if (str[0] == 0)
+		return (0);
+	i = 0;
+	i = ft_strlen(str);
+	to_return = malloc(sizeof(char) * i + 1);
+	if (!to_return)
+		return (0);
+	to_return[i] = 0;
+	i = 0;
+	while (str[i] != 0)
+	{
+		to_return[i] = str[i];
+		i++;
+	}
+	return (to_return);
+}
+
+static char	*to_be_free(char *str)
+{
+	free(str);
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
 	int			jump;
@@ -117,20 +150,53 @@ char	*get_next_line(int fd)
 	char		*str;
 	static char	*container;
 
+	/*if (!fd)
+		return (0);*/
 	bites = 1;
 	jump = 0;
 	str = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!container)
+	{
 		container = malloc(sizeof(char) * BUFFER_SIZE + 1);
+		//printf("\nRESERVA MALLOC CONTAINER\n");
+	}
 	if (!str || !container)
+	{
+		//printf("a");
 		return (0);
-	while (bites > 0 && check_container(container) == 0)
+	}
+	while ((bites > 0 && check_container(container) == 0))
 	{
 		bites = read(fd, str, BUFFER_SIZE);
+		//printf("\n%d\n", bites);
+		/*if (bites == 0)
+		{
+			if (container[0] == 0)
+			{
+				printf("\na\n");
+				return (0);
+			}
+			str = adeblas(container);
+			free(container);
+			return (str);
+		}*/
+		if (bites <= 0)
+		{
+			//printf("\nbites 0\n");
+			break ;
+		}
 		str[bites] = 0;
 		container = ft_strjoin(container, str);
 	}
 	free(str);
+	//printf("\nFLG\n");
+	if (container[0] == 0)
+	{
+		//printf("\nCONTAINER[0]==0\n");
+		free(container);
+		return (0);
+	}
+	//printf("\nFLG2\n");
 	if (check_container(container) != 0)
 	{
 		jump = check_container(container);
@@ -138,30 +204,34 @@ char	*get_next_line(int fd)
 		container = new_container(container, jump);
 		return (line_return);
 	}
-	return (0);
+	//printf("\nFLG3\n");
+	line_return = adeblas(container);
+	container = to_be_free(container);
+	return (line_return);
 }
 /*
 void leaks()
 {
 	system("leaks -q a.out");
-}
+}*/
 
-int main(void)
+/*
+int	main(void)
 {
 	int		fd;
-	int		xd = 0;
 	char	*str;
 
-	atexit(leaks);
-	fd = open("LINES", O_RDONLY);
+	//atexit(leaks);
+	fd = open("./testgnl/files/41_no_nl", O_RDONLY);
 	str = get_next_line(fd);
 	while (str != 0)
 	{
-		printf("%s", str);
+		printf("++%s++", str);
 		free(str);
 		str = get_next_line(fd);
 	}
 	free(str);
+	close(fd);
+	//system("leaks -q a.out");
 	return (0);
-}
-*/
+}*/
---
+++ random.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char	*teesting(char *str)
+{
+	char	*aux;
+
+	aux = malloc (4 + 1);
+	aux[0] = '4';
+	aux[1] = '3';
+	aux[2] = '2';
+	aux[3] = '1';
+	aux[4] = 0;
+	free(str);
+	return (aux);
+}
+
+void leaks()
+{
+	system("leaks -q a.out");
+}
+
+static char	*to_be_free(char *str)
+{
+	free(str);
+	return (0);
+}
+
+int	main(void)
+{
+	static char	*str;
+
+	atexit(leaks);
+	if (!str)
+	{
+		printf("a");
+	}
+	str = malloc (sizeof(char) * 4 + 1);
+	if (str)
+	{
+		printf("b");
+	}
+	str[0] = '1';
+	str[1] = '2';
+	str[2] = '3';
+	str[3] = '4';
+	str[4] = 0;
+	printf("\n%s\n", str);
+	str = teesting(str);
+	printf("\n%s\n", str);
+	if (str[0] == 0)
+		printf("c");
+	free(str);
+	printf("\n%s\n", str);
+	if (str[0] == 0)
+		printf("d");
+	printf("%s", str);
+	//str = to_be_free(str);
+	if (str[0] == 0)
+		printf("e");
+	return (0);
+}
